ed0bb2c1ef353107c18cda393cc6f09b48450b94
darkopuflovic <darkopuflovic@gmail.com>
2020-11-30 01:11:23 +0100

    Ovo je inicijalni projekat

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumbersFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
